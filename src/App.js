d58eede251590de363fd4fc04d7f0b5dd09bd547
anisham-cd <104071170+anisham-cd@users.noreply.github.com>
2022-06-30 17:48:07 +0530

    page creation
    created a page with alert message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  function submit(){
+    console.log("Given details entered")
+    alert("Successfully Entered")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <div className='App'>
+
+      <div>
+        <label>FirstName:</label>
+        <input type='text' />
+      </div>
+      <div>
+        <label>LastName:</label>
+        <input type='text' />
+      </div>
+      <div>
+        <label>Role:</label>
+        <select id="role" name="Role">
+          <option value="Manager">Manager</option>
+          <option value="Developer">Developer</option>
+          <option value="engineer">engineer</option>
+        </select>
+      </div>
+      <script src="index.js"></script>
+
+      <div>
+        <button onClick={submit}>Submit</button> 
+      </div>
+
     </div>
+
+
   );
 }
 
